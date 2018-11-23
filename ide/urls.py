d10486c9cad99a91779a96d8f9705088542b1272
Sayam Kanwar <sayamkanwar616@gmail.com>
2018-11-24 01:21:22 +0530

    Update urls.py

--- ide/urls.py
+++ ide/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from views import index, calculate_parameter, fetch_layer_shape
-from views import load_from_db, save_to_db, fetch_model_history
+from views import load_from_db, loadModel_from_db, deleteModel_from_db, save_to_db, saveModel_to_db, fetch_model_history
 
 urlpatterns = [
     url(r'^$', index),
@@ -14,7 +14,10 @@ urlpatterns = [
     url(r'^keras/', include('keras_app.urls')),
     url(r'^tensorflow/', include('tensorflow_app.urls')),
     url(r'^save$', save_to_db, name='saveDB'),
+    url(r'^saveModel$', saveModel_to_db, name='saveModel'),
     url(r'^load*', load_from_db, name='loadDB'),
+    url(r'^deleteModel$', deleteModel_from_db, name='deleteModel'),
+    url(r'^getModel$', loadModel_from_db, name='getModelData'),
     url(r'^model_history', fetch_model_history, name='model-history'),
     url(r'^model_parameter/', calculate_parameter, name='calculate-parameter'),
     url(r'^layer_parameter/', fetch_layer_shape, name='fetch-layer-shape')
