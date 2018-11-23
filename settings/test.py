f8499c1a24d18f18e567b1d4c3d17fc104578784
Sayam Kanwar <sayamkanwar616@gmail.com>
2018-11-24 01:54:35 +0530

    Update test.py

--- settings/test.py
+++ settings/test.py
@@ -8,10 +8,10 @@ DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'fabrik',
-        'USER': 'admin',
-        'PASSWORD': 'fabrik',
-        'HOST': 'localhost',
+        'NAME': 'fabrik', # Change this to 'postgres' if you're using docker
+        'USER': 'admin', # Change this to 'postgres' if you're using docker
+        'PASSWORD': 'fabrik', # Change this to 'postgres' if you're using docker
+        'HOST': 'localhost', # Change this to 'db' if you're using docker
         'PORT': 5432,
     }
 }
