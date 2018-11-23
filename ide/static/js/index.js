0bf3a0cccbd4f979fd9cda0d11d08d123bdc9c12
Sayam Kanwar <sayamkanwar616@gmail.com>
2018-11-24 01:25:45 +0530

    Update index.js

--- ide/static/js/index.js
+++ ide/static/js/index.js
@@ -1,12 +1,13 @@
-
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 import App from './app.js';
+import Dashboard from './dashboard.js';
 import '../css/style.css';
 
 render(
   <Router history={hashHistory}>
     <Route path="/" component={App} />
+    <Route path="/dashboard" component={Dashboard} />
   </Router>, document.getElementById('app')
 );
