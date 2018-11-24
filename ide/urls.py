08b409024616cc840b3760aa44e9d8753f1f6ba7
Sayam Kanwar <sayamkanwar616@gmail.com>
2018-11-24 14:58:06 +0530

    Update urls.py

--- ide/urls.py
+++ ide/urls.py
@@ -4,7 +4,9 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from views import index, calculate_parameter, fetch_layer_shape
-from views import load_from_db, loadModel_from_db, deleteModel_from_db, save_to_db, saveModel_to_db, fetch_model_history
+from views import load_from_db, load_model_from_db, \
+    delete_model_from_db, save_to_db, save_model_to_db, \
+    fetch_model_history
 
 urlpatterns = [
     url(r'^$', index),
@@ -15,12 +17,15 @@ urlpatterns = [
     url(r'^keras/', include('keras_app.urls')),
     url(r'^tensorflow/', include('tensorflow_app.urls')),
     url(r'^save$', save_to_db, name='saveDB'),
-    url(r'^saveModel$', saveModel_to_db, name='saveModel'),
+    url(r'^saveModel$', save_model_to_db, name='saveModel'),
     url(r'^load*', load_from_db, name='loadDB'),
-    url(r'^deleteModel$', deleteModel_from_db, name='deleteModel'),
-    url(r'^getModel$', loadModel_from_db, name='getModelData'),
+    url(r'^deleteModel$', delete_model_from_db, name='deleteModel'),
+    url(r'^getModel$', load_model_from_db, name='getModelData'),
     url(r'^model_history', fetch_model_history, name='model-history'),
-    url(r'^model_parameter/', calculate_parameter, name='calculate-parameter'),
-    url(r'^layer_parameter/', fetch_layer_shape, name='fetch-layer-shape')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    url(r'^model_parameter/', calculate_parameter,
+        name='calculate-parameter'),
+    url(r'^layer_parameter/', fetch_layer_shape,
+        name='fetch-layer-shape'),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+        + static(settings.STATIC_URL,
+                 document_root=settings.STATIC_ROOT)
