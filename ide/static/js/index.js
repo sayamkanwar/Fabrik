d17d5659d0e1122f5181e5588d66d48e1a206400
Sayam Kanwar <sayamkanwar616@gmail.com>
2018-12-08 19:26:20 +0530

    Update index.js

--- ide/static/js/index.js
+++ ide/static/js/index.js
@@ -3,10 +3,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 import App from './app.js';
+import Home from './home.js';
 import '../css/style.css';
 
 render(
   <Router history={hashHistory}>
     <Route path="/" component={App} />
+    <Route path="/home" component={Home} />
   </Router>, document.getElementById('app')
 );
