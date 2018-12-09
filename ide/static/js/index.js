3ab0419d67bc32e457dab77a224412fa5d299dd0
sayamkanwar <sayamkanwar616@gmail.com>
2018-12-10 00:50:45 +0530

    Revert "Update index.js"
    This reverts commit d6b7a6dabd13d3dfe5dba6754054baf97fd6c91f.

--- ide/static/js/index.js
+++ ide/static/js/index.js
@@ -3,12 +3,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 import App from './app.js';
-import Home from './home.js';
 import '../css/style.css';
 
 render(
   <Router history={hashHistory}>
     <Route path="/" component={App} />
-    <Route path="/home" component={Home} />
   </Router>, document.getElementById('app')
 );
